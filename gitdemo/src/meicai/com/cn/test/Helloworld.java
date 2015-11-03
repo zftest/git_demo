efbf444521dbb22125b8f720f0a26ceaff9a0bef
zhoufeng <zhoufeng@qq.com>
2015-11-03 14:41:28 +0800

    第一次提交代码

---
+++ gitdemo/src/meicai/com/cn/test/Helloworld.java
@@ -0,0 +1,10 @@
+package meicai.com.cn.test;
+
+public class Helloworld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         System.out.println("just to test");
+	}
+
+}
